0193a2c193931417be71bd059c9139cf2d34991b
nooodl <nooodl@ubuntu.ubuntu-domain>
2013-08-01 02:54:25 +0200

    Improve command 'tokenization'

--- hist.py
+++ hist.py
@@ -2,14 +2,14 @@ from collections import Counter
 from itertools import takewhile
 import sys, re
 
-def token(s):
-    if re.search(r'[A-Z0-9\'\"\?]', s): return False
-    return True
-
 t = []
 for s in sys.stdin:
-    if s[0] == '*': print s.strip()
-    t.append(' '.join(list(takewhile(token, s.split()))))
+    s = re.sub(r'''['"].*''', '', s)
+    s = re.sub(r' (for|to|is|at|from|then).*', '', s)
+    s = re.sub(r' [0-9].*', '', s)
+    s = re.sub(r' c[0-9a-f\?]{2}.*', '', s)
+    s = re.sub(r'\s+$', '', s)
+    t.append(s)
 
 for s, c in Counter(t).most_common():
     print '%d\t"%s"' % (c, s)
